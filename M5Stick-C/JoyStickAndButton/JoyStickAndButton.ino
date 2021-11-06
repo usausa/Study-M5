bda517ce598f61c0d5f81d434939d25f7fe6b031
usausa <machi.pon@gmail.com>
2021-11-06 15:25:03 +0900

    Add joystick sample

---
+++ M5Stick-C/JoyStickAndButton/JoyStickAndButton.ino
@@ -0,0 +1,26 @@
+#include <M5StickC.h>
+
+#define JOY_ADDR 0x52 // Joystick I2C address
+
+char data[100];
+
+void setup() {
+    M5.begin();
+    M5.Lcd.setRotation(3);
+    Wire.begin(32, 33);
+}
+
+void loop() {
+    static uint8_t x, y, button;
+
+    Wire.requestFrom(JOY_ADDR, 3);
+    if (Wire.available()) {
+        x = Wire.read();
+        y = Wire.read();
+        button = Wire.read();
+        
+        M5.Lcd.setCursor(1, 30, 2);
+        M5.Lcd.printf("x %03d, y %03d, button %d", x, y, button);
+    }
+    delay(200);
+}
